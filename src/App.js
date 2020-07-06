b59a8f9c4bb38fbbca899d7dc7825097110b9507
Jake Pressman <jpress7639@gmail.com>
2020-07-06 11:10:53 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -28,11 +28,6 @@ function App() {
     </ul>
   </nav>
 </header>
-<div class="cloud x1"></div>
-	<div class="cloud x2"></div>
-	<div class="cloud x3"></div>
-	<div class="cloud x4"></div>
-	<div class="cloud x5"></div>
 <div className="sun"></div>
 <Route path="/" exact>
 <div className="intro">
